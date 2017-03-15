d8ac11de7f36b0ab851c575eab87845349c86f26
Denis Efremov <efremov.a.denis@gmail.com>
2017-03-15 06:17:19 +0300

    Fixed code style and performance

--- resources/js/input.js
+++ resources/js/input.js
@@ -1,28 +1,26 @@
 $(document).on('ajaxComplete ready', function () {
-
     // Initialize the encrypted inputs.
-    $('input[data-provides="anomaly.field_type.encrypted"]:not([data-initialized])').each(function () {
+    $('input[data-provides="anomaly.field_type.encrypted"]:not([data-initialized])')
+        .each(function () {
+            var $this = $(this);
+            var $wrapper = $this.closest('div');
 
-        var input = $(this);
-        var wrapper = input.closest('div');
+            $this.attr('data-initialized', '');
 
-        input.attr('data-initialized', '');
+            $wrapper.find('[data-toggle="text"]').click(function (e) {
+                e.preventDefault();
 
-        wrapper.find('[data-toggle="text"]').click(function (e) {
-            
-            e.preventDefault();
+                $(this).find('i')
+                    .toggleClass('fa-toggle-on')
+                    .toggleClass('fa-toggle-off');
 
-            $(this).find('i')
-                .toggleClass('fa-toggle-on')
-                .toggleClass('fa-toggle-off');
+                if ($this.attr('type') === 'password') {
+                    $this.attr('type', 'text').focus();
+                } else {
+                    $this.attr('type', 'password').focus();
+                }
 
-            if (input.attr('type') == 'password') {
-                input.attr('type', 'text').focus();
-            } else {
-                input.attr('type', 'password').focus();
-            }
-
-            return false;
+                return false;
+            });
         });
-    });
 });
